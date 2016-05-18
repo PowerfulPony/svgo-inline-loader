714e38481e16fe0e5ddd44c8dc68c1cc16e99961
Victor Zakharenko <ponymtb@gmail.com>
2016-05-18 12:26:30 +0300

    SVG loader inline for WebPack
    query: remove: [...string]

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var loaderUtils = require('loader-utils');
+var SVGO = require('svgo');
+var svgo = new SVGO();
+
+module.exports = function(content) {
+  this.cacheable && this.cacheable();
+
+  var output = '' + content;
+
+  var query = loaderUtils.parseQuery(this.query);
+
+  if (query && query.remove) {
+    for (var i = query.remove.length - 1; i >= 0; i--) {
+      var removable = query.remove[i];
+      var re = new RegExp(removable, 'gim');
+      output = output.replace(re, '');
+    }
+  }
+
+  svgo.optimize(output, function(result) {
+    output = result.data;
+  })
+
+  return "module.exports = '" + output + "'";
+}
